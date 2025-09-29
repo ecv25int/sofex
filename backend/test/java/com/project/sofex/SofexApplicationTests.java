a28fa190a89c55350d69640a2c2891017f8b795c
Helbert Cerdas <ecv25int@gmail.com>
2025-09-29 09:25:58 +0100

    Add files via upload

---
+++ backend/test/java/com/project/sofex/SofexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sofex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
